7dd32c41133b44c53207a901ec4cad7b0484fd9b
riadhriadh2004 <29288189-riadhriadh2004@users.noreply.replit.com>
2024-04-03 19:26:12 +0000

    solve the responsive navbar problem

--- src/App.jsx
+++ src/App.jsx
@@ -29,13 +29,15 @@ export default function App() {
   
   return (
     <>
-      <div className='py-2 dark:bg-slate-900 bg-white flex flex-col md:flex-row md:justify-between z-[60]'>
-        <div className='z-[60]'>
+      <div className='dark:bg-slate-900 bg-white flex flex-col md:flex-row md:justify-between'>
+        <div className='z-[60] py-2 dark:bg-slate-900 bg-white'>
+        <div className=''>
           <img src="https://upload.wikimedia.org/wikipedia/commons/thumb/f/f4/BMW_logo_%28gray%29.svg/2048px-BMW_logo_%28gray%29.svg.png" alt="" className='w-16 h-16 ml-7 cursor-pointer hover:sepia duration-150' />
           </div>
         <button onClick={() => setIsOpen(!isOpen)} className='text-3xl text-yellow-600 md:hidden absolute top-6 right-4 z-[60]'>
             <ion-icon name={isOpen ? "close-outline" : "menu-outline"}></ion-icon>
         </button>
+          </div>
         <ul className={`flex flex-col md:flex-row space-y-3 md:space-y-0 md:space-x-3 mr-7 items-center absolute md:static border-t-2 border-blue-500 md:border-none dark:bg-slate-900 bg-white p-3 md:p-0 w-full md:transition-none transition-all duration-500 md:w-auto ${isOpen ? 'top-20 opacity-100 z-[50]' : 'opacity-0 md:top-0 md:opacity-100 top-[-200px] z-[-1] md:z-auto'}`}>
         {links.map(link => {
           return(
@@ -57,7 +59,7 @@ export default function App() {
       <div className='w-full min-h-screen dark:bg-slate-200 bg-slate-500 relative'>
         <div className='flex justify-center items-center absolute top-4 right-4 rounded-lg bg-white dark:bg-slate-900 overflow-hidden'>
           {options.map((opt) => (
-            <button key={opt.theme} onClick={() => setTheme(opt.theme)} className={`w-6 h-6 m-1 rounded-lg text-black dark:text-white text-lg dark:hover:bg-slate-800 hover:bg-gray-200 ${theme === opt.theme ? 'dark:text-red-500 text-yellow-600' : ''}`}>
+            <button key={opt.theme} onClick={() => setTheme(opt.theme)} className={`w-6 h-6 m-1 rounded-lg text-black dark:text-white text-lg dark:hover:bg-slate-800 hover:bg-gray-200 ${theme === opt.theme ? 'dark:text-blue-500 text-yellow-600' : ''}`}>
               <ion-icon name={opt.icon}></ion-icon>
             </button>
           ))}
